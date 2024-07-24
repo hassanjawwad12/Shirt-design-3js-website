d7b575899922b84dfc790161466f569f27e78714
Muhammad Hassan Jawwad <113373773+hassanjawwad12@users.noreply.github.com>
2024-07-24 21:41:52 +0500

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import CustomButton from "./CustomButton";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tabs from "./Tabs";
+export {CustomButton};
+
+export {ColorPicker};
+
+export {FilePicker};
+
+export {Tabs};
---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import {proxy} from 'valtio'
+
+const state = proxy({
+intro:true ,
+color:'#EFBD48',
+isLogoTextured:true,
+isFullTexture:false,
+logoDecal:'./threejs.png',
+fullDecal:'./threejs.png',
+})
+
+export default state
